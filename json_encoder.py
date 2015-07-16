5b4baf1039b898ef0e83f490ed6627ba4a9461b8
Samuel Gratzl <samuel_gratzl@gmx.at>
2015-07-16 16:44:42 +0200

    move numpy json encoder

--- json_encoder.py
+++ json_encoder.py
@@ -5,23 +5,16 @@ import tables
 
 class NumpyTablesEncoder(object):
   def __contains__(self, obj):
-    if isinstance(obj, np.ndarray) or isinstance(obj, tables.Array):
-      return True
-    if isinstance(obj, np.generic):
+    if isinstance(obj, tables.Array):
       return True
     return False
 
   def __call__(self, obj, base_encoder):
-    if isinstance(obj, np.ndarray) or isinstance(obj, tables.Array):
+    if isinstance(obj, tables.Array):
       if obj.ndim == 1:
         return [x for x in obj]
       else:
         return [base_encoder.default(obj[i]) for i in range(obj.shape[0])]
-    if isinstance(obj, np.generic):
-      a = np.asscalar(obj)
-      if isinstance(a, float) and np.isnan(a):
-        return None
-      return a
     return None
 
 n = NumpyTablesEncoder()
